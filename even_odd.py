5d730106329c2552759ca24fbca166faa0ccc525
zar <zahraocourse@gmail.com>
2025-12-31 23:01:00 +0330

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,9 @@
+# Problem: Check whether a number is even or odd
+# Approach: Use modulo operator and conditional statements
+
+number = int(input("Enter a number: "))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
